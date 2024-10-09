0179147ecde2b6d42a72a398740d7e94e675318d
Darkcoffe1859 <shency49@gmail.com>
2024-10-09 11:13:48 -0400

    portfolio

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 // src/components/Header/Header.js
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
@@ -9,11 +9,16 @@ const Header = () => {
             <h1>My E-Commerce Site</h1>
             <nav>
                 <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/products">Products</Link></li>
-                    <li><Link to="/cart">Cart</Link></li>
+                    <li><NavLink exact to="/" activeClassName="active-link">Home</NavLink></li>
+                    <li><NavLink to="/products" activeClassName="active-link">Products</NavLink></li>
+                    <li><NavLink to="/cart" activeClassName="active-link">Cart</NavLink></li>
+                    <li><NavLink to="/login" activeClassName="active-link">Login</NavLink></li>
                 </ul>
             </nav>
+            <form className="search-form">
+                <input type="text" placeholder="Search products..." />
+                <button type="submit">Search</button>
+            </form>
         </header>
     );
 };
